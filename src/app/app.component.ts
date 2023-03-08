d3609f8494b43e2beef4b6ff0490843cb3597c58
gabrielrios27 <juangrios27@gmail.com>
2023-03-07 22:23:41 -0300

    Feat - Crea hover en iconos de youtube e instagram - Maqueta loader al iniciar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'Libee';
+  startLoader: boolean;
+  constructor() {
+    this.startLoader = true;
+  }
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.startLoader = false;
+    }, 1000);
+  }
 }
